4bb3dafb9395b88a40ec5d7b45c4e9ae53db6194
float321 <187214682+float321@users.noreply.github.com>
2025-02-03 23:43:26 +0300

    Update game.py

--- src/game.py
+++ src/game.py
@@ -4,6 +4,9 @@ from func import load_image, show_image, terminate
 from func import map_generation
 
 
+button_pause = pygame.sprite.Group()
+pause_group = pygame.sprite.Group()
+
 # Загрузка данных из настроек
 def load_settings():
     global SETTINGS
@@ -50,14 +53,15 @@ def interface():
     for i in images:
         show_image(i, screen_game, 'interface')
 
+# Отключено для паузы
+# def check_cursor(cursor_rect):
+#    cursor_x, cursor_y = pygame.mouse.get_pos()
+#    if not cursor_rect.collidepoint(cursor_x, cursor_y):
+#        # Если курсор вышел за границы, возвращаем его обратно
+#        cursor_x = max(cursor_rect.left, min(cursor_x, cursor_rect.right))
+#        cursor_y = max(cursor_rect.top, min(cursor_y, cursor_rect.bottom))
+#        pygame.mouse.set_pos(cursor_x, cursor_y)
 
-def check_cursor(cursor_rect):
-    cursor_x, cursor_y = pygame.mouse.get_pos()
-    if not cursor_rect.collidepoint(cursor_x, cursor_y):
-        # Если курсор вышел за границы, возвращаем его обратно
-        cursor_x = max(cursor_rect.left, min(cursor_x, cursor_rect.right))
-        cursor_y = max(cursor_rect.top, min(cursor_y, cursor_rect.bottom))
-        pygame.mouse.set_pos(cursor_x, cursor_y)
 
 # Обновление изменяемых характеристик героя
 def update_hp_mana_coins(*hp_states, **characteristics):
@@ -67,11 +71,11 @@ def update_hp_mana_coins(*hp_states, **characteristics):
     screen_game.blit(coin_text, (1475, 110))
 
     magic_font = pygame.font.Font('data/shrifts/coins_shrift.ttf', 50)
-    magic_text = magic_font.render(f'{str(characteristics['mana'])}',
+    magic_text = magic_font.render(f'{str(characteristics["mana"])}',
                                    False, (20, 20, 20))
     screen_game.blit(magic_text, (395, 110))
 
-    magic_text = magic_font.render(f'/{str(characteristics['unlocked_mana'])}',
+    magic_text = magic_font.render(f'/{str(characteristics["unlocked_mana"])}',
                                    False, (20, 20, 20))
     screen_game.blit(magic_text, (485, 110))
 
@@ -86,6 +90,34 @@ def update_hp_mana_coins(*hp_states, **characteristics):
     for i in range(10 - characteristics['unlocked_hp']):
         hp_states[2][1] = 1235 - i * 64
         show_image(hp_states[2], screen_game, 'interface')
+
+
+class Button(pygame.sprite.Sprite):
+
+    # Класс Кнопки в общем виде
+    def __init__(self, a, dx, dy, x, y, group):
+        super().__init__(group)
+        self.button_type = a
+        self.image = load_image(self.button_type, 'main')
+        self.image = pygame.transform.scale(self.image, (dx, dy))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, *args):
+        global running, pausing, pause_group
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            if self.button_type == 'points3.png':
+                pause()
+            elif self.button_type == 'continue.png':
+                pausing = False
+                for i in pause_group:
+                    i.kill()
+            elif self.button_type == 'menu_back.png':
+                pygame.quit()
+                os.system('python main.py')
+            elif self.button_type == 'settings.png':
+                pass
 
 
 class Player(pygame.sprite.Sprite):
@@ -104,14 +136,13 @@ class Player(pygame.sprite.Sprite):
         self.walk_animation = 0
 
         self.form = [f'{self.side_animation}/stop',
-                    self.x, self.y, self.width, 120]
+                     self.x, self.y, self.width, 120]
 
         self.image = load_image(f'{self.form[0]}.png', 'characters\main_hero')
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
-
 
         self.characteristics = {'coins': 0,
                                 'hp': 4,
@@ -172,41 +203,39 @@ class Player(pygame.sprite.Sprite):
             sounds['steps'].stop()
 
         self.animation_flag = False
+
     # Взаимодействия
     def action(self, event):
-            if event.key == ord(SETTINGS['interaction']):
-                # Двери
-                can = False
-                if self.rect.x >= 1440 and 400 < self.rect.y < 560:
-                    can = room.change_room_number('right')
-                    if can:
-                        self.rect.x = 350
-                elif self.rect.x <= 360 and 400 < self.rect.y < 550:
-                    can = room.change_room_number('left')
-                    if can:
-                        self.rect.x = 1550 - self.width
-                elif self.rect.y >= 635 and 840 < self.rect.x < 1000:
-                    can = room.change_room_number('down')
-                    if can:
-                        self.rect.y = 190
-                elif self.rect.y <= 195 and 840 < self.rect.x < 1000:
-                    can = room.change_room_number('up')
-                    if can:
-                        self.rect.y = 765 - self.height
+        if event.key == ord(SETTINGS['interaction']):
+            # Двери
+            can = False
+            if self.rect.x >= 1440 and 400 < self.rect.y < 560:
+                can = room.change_room_number('right')
+                if can:
+                    self.rect.x = 350
+            elif self.rect.x <= 360 and 400 < self.rect.y < 550:
+                can = room.change_room_number('left')
+                if can:
+                    self.rect.x = 1550 - self.width
+            elif self.rect.y >= 635 and 840 < self.rect.x < 1000:
+                can = room.change_room_number('down')
+                if can:
+                    self.rect.y = 190
+            elif self.rect.y <= 195 and 840 < self.rect.x < 1000:
+                can = room.change_room_number('up')
+                if can:
+                    self.rect.y = 765 - self.height
 
-                # Сундук
-                if room.this_room[0] == 'chest':
-                    if 770 < self.rect.x < 1100 and 340 < self.rect.y < 610:
-                        chest.animation_flag = True
-                        map_list[room.room_number[0]][room.room_number[1]][1] = 'used'
+            # Сундук
+            if room.this_room[0] == 'chest':
+                if 770 < self.rect.x < 1100 and 340 < self.rect.y < 610:
+                    chest.animation_flag = True
+                    map_list[room.room_number[0]][room.room_number[1]][1] = 'used'
 
-                        
+        elif event.key == pygame.K_z:
+            print(self.rect.x, self.rect.y)
 
-            elif event.key == pygame.K_z:
-                print(self.rect.x, self.rect.y)
-
-
-        # Обновление изменяемых характеристик и картинки героя
+    # Обновление изменяемых характеристик и картинки героя
     def update(self):
         update_hp_mana_coins(*self.hp_states, **self.characteristics)
         show_image(self.form, screen_game, 'characters/main_hero')
@@ -233,18 +262,18 @@ class Object(pygame.sprite.Sprite):
         self.rect.y = y
 
         all_objects.add(self)
+
     def update(self):
         if self.animation_flag and self.animation < self.max_animation:
             self.animation += 1
 
             self.image_fin = load_image(f'{self.image}{self.animation}.png', self.where)
             self.mask = pygame.mask.from_surface(self.image_fin)
-        show_image([f'{self.image}{self.animation}', self.rect.x, self.rect.y, self.width, self.height], screen_game, self.where)
+        show_image([f'{self.image}{self.animation}', self.rect.x, self.rect.y, self.width, self.height], screen_game,
+                   self.where)
 
-    
 
-
-class Room():
+class Room:
     # Инициализация начальных сведений о комнатах
     def __init__(self, map_list, room_number):
         self.room_number = room_number
@@ -263,13 +292,13 @@ class Room():
         if self.this_room[0] == 'chest':
             global chest
             try:
-                if not chest in all_objects:
+                if chest not in all_objects:
                     if self.this_room[1] != 'used':
                         chest = Object('chest_animation_', 'map/chest', 900, 450, 150, 150, 5)
                     else:
                         chest = Object('chest_animation_', 'map/chest', 900, 450, 150, 150, 5)
                         chest.animation = 5
-            except:
+            except Exception:
                 chest = Object('chest_animation_', 'map/chest', 900, 450, 150, 150, 5)
             chest.update()
 
@@ -297,22 +326,34 @@ class Room():
                     self.map_list[self.room_number[0]][self.room_number[1] + 1][0] != 'no'):
                 self.room_number[1] += 1
                 can = True
-        
+
         if can:
             global all_objects
             all_objects = pygame.sprite.Group()
 
             if not sounds['door_open'].get_busy():
                 sounds['door_open'].play(pygame.mixer.Sound('data/music_and_sounds/sounds/map_sounds/door_open.mp3'))
-                                
+
         print(self.room_number)
         return can
+
+
+def pause():
+    global pausing
+    points3 = Button('points3.png', 250, 100, 1620, 20, button_pause)
+    cont = Button('continue.png', 300, 100, 800, 220, pause_group)
+    menu_back = Button('menu_back.png', 300, 100, 800, 400, pause_group)
+    settings_pause = Button('settings.png', 300, 100, 800, 500, pause_group)
+    pausing = True
+
 
 # Начало программы
 def start():
     global screen_game, room
     global all_borders, all_objects
     global sounds, map_list
+    global running, pausing
+    global pause_group
 
     pygame.init()
     # Создание экрана
@@ -331,6 +372,12 @@ def start():
 
     sounds['steps'] = pygame.mixer.find_channel()
     sounds['door_open'] = pygame.mixer.find_channel()
+
+    points3 = Button('points3.png', 250, 100, 1620, 20, button_pause)
+    cont = Button('continue.png', 300, 100, 800, 220, pause_group)
+    menu_back = Button('menu_back.png', 300, 100, 800, 400, pause_group)
+    settings_pause = Button('settings.png', 300, 100, 800, 500, pause_group)
+    pausing = False
 
     # Отрисовка интерфейса и генерация карты уровня
     interface()
@@ -360,24 +407,33 @@ def start():
     while running:
         # Выход из программы
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                terminate()
+            if not pausing:
+                if event.type == pygame.QUIT:
+                    terminate()
 
-            if event.type == pygame.KEYUP:
-                player.action(event)
+                if event.type == pygame.KEYUP:
+                    player.action(event)
+                button_pause.update(event)
+            else:
+                if event.type == pygame.QUIT:
+                    terminate()
 
+                pause_group.update(event)
         # Загрузка настроек, создание комнтаты, обновление персонажа
-        load_settings()
-        check_cursor(cursor_rect)
-        room.create()
-        player.movement()
-        player.update()
-        all_borders.draw(screen_game)
-
-        clock.tick(FPS)
-        pygame.display.flip()
-
+        if not pausing:
+            load_settings()
+    #        check_cursor(cursor_rect)
+            room.create()
+            player.movement()
+            player.update()
+            all_borders.draw(screen_game)
+            button_pause.draw(screen_game)
+            clock.tick(FPS)
+            pygame.display.flip()
+        else:
+            pygame.draw.rect(screen_game, 'Black', (600, 200, 700, 700), 0)
+            pause_group.draw(screen_game)
+            pygame.display.flip()
     pygame.quit()
 
 
